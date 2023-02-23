268558c57ed0ea69fa6e1edb76d0a41ee27ad2c4
hnggngn <rozephyrus@gmail.com>
2023-02-23 11:37:25 +0700

    fix: missing url path

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -6,7 +6,7 @@ export const trpc = createTRPCProxyClient<IAppRouter>({
     httpBatchLink({
       url: import.meta.env.DEV
         ? "http://127.0.0.1:3000/api/trpc"
-        : import.meta.env.PUBLIC_APP_URL,
+        : `${import.meta.env.PUBLIC_APP_URL}/api/trpc`,
     }),
   ],
 });
