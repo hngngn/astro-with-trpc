94c512f894fc935f02683b7f1a173db94e6b4076
hnggngn <rozephyrus@gmail.com>
2023-02-24 15:09:33 +0700

    fix: url

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -4,9 +4,7 @@ import type { IAppRouter } from "../server";
 export const trpc = createTRPCProxyClient<IAppRouter>({
   links: [
     httpBatchLink({
-      url: import.meta.env.DEV
-        ? "http://127.0.0.1:3000/api"
-        : `${import.meta.env.PUBLIC_APP_URL}/api`,
+      url: "/api",
     }),
   ],
 });
